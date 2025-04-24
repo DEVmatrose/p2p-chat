fa3fe90e90522af482130b7863cd9e4c413d1dd2
onlinedemo <onlinedemo@protonmail.com>
2025-04-24 10:53:58 +0000

    fix: update signaling server url

--- src/services/signaling.js
+++ src/services/signaling.js
@@ -1,37 +1,22 @@
-// src/services/signaling.js
-// Hier implementierst du die Kommunikation mit dem Signaling-Server
-
-import io from 'socket.io-client';
+// src/signaling.js
+import { io } from 'socket.io-client';
 
 class Signaling {
-  constructor(url) {
-    this.socket = io(url);
+    constructor() {
+        this.url = 'https://p2p-chat-signaling.onrender.com/';
+        this.socket = io(this.url);
 
-    this.socket.on('connect', () => {
-      console.log('Connected to signaling server');
-      // Hier kannst du Logik für die verbundene Verbindung einfügen
-    });
+        this.socket.on('connect', () => {
+            console.log('connected to signaling server');
+        });
+        this.socket.on('disconnect', () => {
+            console.log('disconnected from signaling server');
+        });
+    }
 
-    this.socket.on('disconnect', () => {
-      console.log('Disconnected from signaling server');
-      // Hier kannst du Logik für die getrennte Verbindung einfügen
-    });
-
-    this.socket.on('error', (error) => {
-      console.error('Signaling server error:', error);
-      // Hier kannst du Logik für Fehler einfügen
-    });
-
-    this.socket.on('message', (message) => {
-        console.log('Message from signaling server:', message);
-        // Hier wird die Logik für Nachrichten vom Signaling-Server eingebaut
-    })
-  }
-
-  // Hier implementierst du Methoden, um Nachrichten zu senden und zu empfangen
-  sendMessage(message) {
-    this.socket.emit('message', message);
-  }
+    sendMessage(message) {
+        this.socket.emit('message', message);
+    }
 }
 
 export default Signaling;
